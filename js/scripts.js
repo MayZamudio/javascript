8ec400fdc193753ef129b5abc6db7025da8e58dc
Mayra Zamudio <mayperal89@gmail.com>
2018-04-09 20:59:41 -0700

    worked on vars

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var myHistory = "I come from the retail industy and am looking to change industries";
+console.log(myHistory);
+var myValue = confirm(myHistory);
+console.log(myValue);
+
+// capture the ture/false
