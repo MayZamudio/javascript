4ae01e6e8bb34533820caecc84b8c0e9568ef5fe
Mayra Zamudio <mayperal89@gmail.com>
2018-04-10 19:45:33 -0700

    added embed

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,4 @@
 var myHobby = "I love to watch re-runs of Big Bang Theory with my 10 year old daughter.";
 console.log(myHobby);
 var myValue = confirm(myHobby);
-console.log(myValue)
+console.log(myValue);
