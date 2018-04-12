c63af00259fd32fb0825dcc855a2996418acf663
Mayra Zamudio <mayperal89@gmail.com>
2018-04-12 15:39:09 -0700

    worked on vars, let, const, and console

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+// console.log(myName);
+// var myName = "Mayra Zamudio";
+// myName = new String(myName);
+// console.log(myName.toUpperCase());
+//
+// var myAge = 28;
+// myAge = new Number(myAge);
+// console.log(typeof (myAge));
+//
+// var myAge = 28;
+// myAge = new Number(myAge);
+// console.log(myAge);
+//
+// var test; //Declared
+// test = "My test is awesome" //Defined
+// console.log(test);
+
+
+const myName = "Mayra";
+console.log(myName);
+ {
+  const myName = "Crystal";
+  console.log(myName);
+};
+console.log(myName)
+
+let birthday = "9/07/07";
+let rightNow = new Date(birthday);
+
+alert(rightNow);
+
+// MATH OBJECT
+alert(Math.PI);
+
+// alert 1 option used for telling user something
+alert("Alert");
+//Confirm
+const isOfAge = confirm("Are you 21 or older");
+
+//Prompt
+const userStory = prompt("Tell me do you take walks?");
+
+console.log(userStory);
