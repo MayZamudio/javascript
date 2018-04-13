0ad2e43b45657738c61c59e884bc0a0df573212c
Mayra Zamudio <mayperal89@gmail.com>
2018-04-13 13:18:09 -0700

    capitals

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
-let firstName = prompt("What is your first name?");
-let lastName = prompt("What is your last name?");
+let firstName = prompt("What is your First Name?");
+let lastName = prompt("What is your Last Name?");
+console.log(firstName);
 alert("So your name is " +firstName+" "+lastName);
 let birthDate = prompt("What is your birthday? ");
 confirm("So your birthday is "+ birthDate); //if oy are combinig two strings you need to +
