9f5918373a6075b46f113428911486c49e542360
Mayra Zamudio <mayperal89@gmail.com>
2018-04-13 13:01:00 -0700

    take home

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let firstName = prompt("What is your first name?");
+let lastName = prompt("What is your last name?");
+alert("So your name is " +firstName+" "+lastName);
+let birthDate = prompt("What is your birthday? ");
+confirm("So your birthday is "+ birthDate); //if oy are combinig two strings you need to +
