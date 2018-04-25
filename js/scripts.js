6e15173061c79c2cbfb78ad7d1b79036f0160f91
Mayra Zamudio <mayperal89@gmail.com>
2018-04-24 17:12:53 -0700

    day5

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,24 @@
 console.log("Hello World!")
 // You call a function and the function returns a value (printed string in the console)
 
-function sayHello(name, greeting){
+function sayHello(name, greeting){  //FUNCTION DEFINITION
   return `Hello ${name}, ${greeting}!`;
 }
 
-let myGreeting = sayHello("Mayra","You're the best!");
+let myGreeting = sayHello("Mayra","You're the best!");   //CALL SITE
 
 alert(myGreeting);
+
+//FUNCTION EXPRESSION:
+var logStuff = function(param) {
+   console.log(`${param} was passed in`);
+};
+
+//FUNCTION DECLARATION
+function multiply(num, secondNum) {
+    return num * secondNum;
+};
+//****IMPORTANT: Function hoisting only works with function declaration and not with function expression.
+
+
+var fav = "cheese"
